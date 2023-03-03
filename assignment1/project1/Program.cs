7cd9e140c5a62dd94d7f9729b60a308d0214d090
金龙 杨 <668008@163.com>
2023-03-03 09:37:40 +0800

    Finish Project1

--- assignment1/project1/Program.cs
+++ assignment1/project1/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string x = Console.ReadLine();
+int operatenum1 = int.Parse(x);
+string y = Console.ReadLine();
+int operatenum2 = int.Parse(y);
+char operatorSymbol = (char)Console.Read();
+switch (operatorSymbol)
+{
+    case '+':
+    case '-':
+    case '*':
+    case '/':
+        {
+            switch (operatorSymbol)
+            {
+                case '+':
+                    int resultplus = operatenum1 + operatenum2;
+                    Console.WriteLine(operatenum1 + "+" + operatenum2 + "=" + resultplus);
+                    break;
+                case '-':
+                    int resultminus = operatenum1 - operatenum2;
+                    Console.WriteLine(operatenum1 + "-" + operatenum2 + "=" + resultminus);
+                    break;
+                case '*':
+                    int resultmulti = operatenum1 * operatenum2;
+                    Console.WriteLine(operatenum1 + "*" + operatenum2 + "=" + resultmulti);
+                    break;
+                case '/':
+                    int resultdivision = operatenum1 / operatenum2;
+                    Console.WriteLine(operatenum1 + "/" + operatenum2 + "=" + resultdivision);
+                    break;
+                default:
+                    break;
+            }
+
+            break;
+        }
+
+    default:
+        Console.WriteLine("没有相应的运算符");
+        break;
+}
