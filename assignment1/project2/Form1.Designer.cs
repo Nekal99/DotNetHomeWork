86e0fda7f27b5236969e1b04cd6623cc9114d07d
金龙 杨 <668008@163.com>
2023-03-03 10:30:59 +0800

    Finish Project2

--- assignment1/project2/Form1.Designer.cs
+++ assignment1/project2/Form1.Designer.cs
@@ -28,12 +28,78 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            operateNum1 = new TextBox();
+            operateNum2 = new TextBox();
+            caculate = new Button();
+            result = new Label();
+            calOperator = new TextBox();
+            SuspendLayout();
+            // 
+            // operateNum1
+            // 
+            operateNum1.Location = new Point(381, 212);
+            operateNum1.Name = "operateNum1";
+            operateNum1.PlaceholderText = "请输入整数操作数1";
+            operateNum1.Size = new Size(276, 38);
+            operateNum1.TabIndex = 0;
+            // 
+            // operateNum2
+            // 
+            operateNum2.Location = new Point(790, 212);
+            operateNum2.Name = "operateNum2";
+            operateNum2.PlaceholderText = "请输入整数操作数2";
+            operateNum2.Size = new Size(291, 38);
+            operateNum2.TabIndex = 1;
+            // 
+            // caculate
+            // 
+            caculate.Location = new Point(650, 377);
+            caculate.Name = "caculate";
+            caculate.Size = new Size(150, 46);
+            caculate.TabIndex = 2;
+            caculate.Text = "Caculate";
+            caculate.UseVisualStyleBackColor = true;
+            caculate.Click += caculate_Click;
+            // 
+            // result
+            // 
+            result.AutoSize = true;
+            result.Location = new Point(671, 473);
+            result.Name = "result";
+            result.Size = new Size(110, 31);
+            result.TabIndex = 3;
+            result.Text = "操作结果";
+            // 
+            // calOperator
+            // 
+            calOperator.Location = new Point(613, 309);
+            calOperator.Name = "calOperator";
+            calOperator.PlaceholderText = "请输入四则运算符";
+            calOperator.Size = new Size(216, 38);
+            calOperator.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(14F, 31F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1458, 987);
+            Controls.Add(calOperator);
+            Controls.Add(result);
+            Controls.Add(caculate);
+            Controls.Add(operateNum2);
+            Controls.Add(operateNum1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox operateNum1;
+        private TextBox operateNum2;
+        private Button caculate;
+        private Label result;
+        private TextBox calOperator;
     }
 }
--- assignment1/project2/Form1.cs
+++ assignment1/project2/Form1.cs
@@ -6,5 +6,47 @@ namespace project2
         {
             InitializeComponent();
         }
+
+        private void caculate_Click(object sender, EventArgs e)
+        {
+            int value1 = Convert.ToInt32(operateNum1.Text);
+            int value2 = Convert.ToInt32(operateNum2.Text);
+            switch (calOperator.Text)
+            {
+                case "+":
+                case "-":
+                case "*":
+                case "/":
+                    {
+                        switch (calOperator.Text)
+                        {
+                            case "+":
+                                int resultplus = value1 + value2;
+                                result.Text = Convert.ToString(resultplus);
+                                break;
+                            case "-":
+                                int resultminus = value1 - value2;
+                                result.Text = Convert.ToString(resultminus);
+                                break;
+                            case "*":
+                                int resultmulti = value1 * value2;
+                                result.Text = Convert.ToString(resultmulti);
+                                break;
+                            case "/":
+                                int resultdivision = value1 / value2;
+                                result.Text = Convert.ToString(resultdivision);
+                                break;
+                            default:
+                                break;
+                        }
+                        break;
+                    }
+
+                default:
+                    result.Text = "‘ÀÀ„ ß∞‹";
+                    break;
+            }
+
+        }
     }
 }
