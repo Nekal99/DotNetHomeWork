903d2fd6ee31e8c9a7a31362e286f7fefec3ae36
金龙 杨 <668008@163.com>
2023-03-17 11:43:50 +0800

    Finish Assignment4

---
+++ assignment4/AlarmClock/Program.cs
@@ -0,0 +1,90 @@
+﻿public class AlarmClockArgs : EventArgs
+{
+    private int hour;
+    private int minute;
+    private int second;
+    public int Hour
+    { get { return hour; } set { hour = value; } }
+    public int Minute
+    { get { return minute; } set { minute = value; } }
+    public int Second
+    { get { return second; } set {  second = value; } }
+}
+
+public delegate void AlarmClockHandler(object sender,AlarmClockArgs args);
+
+public class AlarmClock
+{
+    static int hour = 0;
+    static int min = 0;
+    static int second = 0;
+    public event AlarmClockHandler? Ticking;
+    public event AlarmClockHandler? Alarming;
+
+    public void start(int setHour,int setMin ,int setSecond)
+    {
+        while (hour<=1)
+        {
+            while (min<60)
+            {
+                while (second < 60)
+                {
+                    if (Ticking != null)
+                    {
+                        AlarmClockArgs args = new AlarmClockArgs();
+                        args.Hour = hour;
+                        args.Minute = min;
+                        args.Second = second;
+                        Ticking(this, args);
+                    }
+                    Alarm(setHour, setMin, setSecond);
+                    second++;
+                }
+                min++;
+                second = 0;
+            }
+            hour++;
+            min = 0; 
+            second = 0;
+        }
+    }
+
+    public void Alarm(int setHour, int setMin, int setSecond)
+    {
+        if(setHour<24&&setHour>=0&&setMin<60&&setMin>=0&&setSecond>=0&&setSecond<60)
+        {
+            if(setHour==hour&&setMin==min&&setSecond==second)
+            {
+                if (Alarming != null)
+                {
+                    AlarmClockArgs args = new AlarmClockArgs();
+                    args.Hour = hour;
+                    args.Minute = min;
+                    args.Second = second;
+                    Alarming(this, args);
+                }
+            }
+        }
+    }
+}
+
+public class UseAlarmClock
+{
+    static void ShowTime(object sender, AlarmClockArgs e)
+    {
+        Console.WriteLine(e.Hour+":"+e.Minute+":"+e.Second);
+    }
+
+    static void AlaumBing(object sender, AlarmClockArgs e)
+    {
+        Console.WriteLine("album bing bing ");
+    }
+    static void Main()
+    {
+        var alarmClock = new AlarmClock();
+        alarmClock.Ticking += ShowTime;
+        alarmClock.Alarming += AlaumBing;
+        Console.WriteLine("时钟开始滴答");
+        alarmClock.start(0,0,20);//参数为设置的时钟响铃时间
+    }
+}
---
+++ assignment4/generics/Program.cs
@@ -0,0 +1,110 @@
+﻿public class Node<T>
+{
+    public Node<T>? Next { get; set; }
+    public T Data { get; set; }
+
+    public Node(T t)
+    {
+        this.Next = null;
+        this.Data = t;
+    }
+    public Node(Node<T> t)
+    {
+        this.Next = t.Next;
+        this.Data = t.Data;
+    }
+}
+
+public class GenericList<T>
+{
+    private Node<T>? head;
+    private Node<T>? tail;
+
+    public Node<T>? Head
+    {
+        get
+        {
+            return head;
+        }
+        private set
+        {
+            head = value;
+        }
+    }
+
+    public Node<T>? Tail
+    {
+        get
+        {
+            return tail;
+        }
+        private set
+        {
+            tail = value;
+        }
+    }
+
+    public GenericList()
+    {
+        head = tail = null;
+    }
+
+    public void Add(T t)
+    {
+        Node<T> n = new(t);
+        if (tail == null)
+        {
+            head = tail = n;
+        }
+        else
+        {
+            tail.Next = n;
+            tail = n;
+        }
+    }
+
+    public void ForEach(Action<Node<T>> action)
+    {
+
+        for (Node<T>? i = head; i != null; i = i.Next)
+        {
+            action(i);
+        }
+    }
+}
+
+public class Test
+{
+    static void Main()
+    {
+        GenericList<int> genericList = new GenericList<int>();
+        int max = int.MinValue;
+        int min = int.MaxValue;
+        int sum = 0;
+
+        for (int i = 0; i < 10; i++)
+        {
+            genericList.Add(i);
+        }
+
+        Console.WriteLine("泛型链表的各个元素为值");
+
+        genericList.ForEach(s => Console.WriteLine(s.Data));
+        genericList.ForEach(s =>
+        {
+            sum += s.Data;
+            if (max <= s.Data)
+            {
+                max = s.Data;
+            }
+            if (min >= s.Data)
+            {
+                min = s.Data;
+            }
+        });
+
+        Console.WriteLine("泛型链表的和为" + sum);
+        Console.WriteLine("最大值为" + max);
+        Console.WriteLine("最小值为" + min);
+    }
+}
